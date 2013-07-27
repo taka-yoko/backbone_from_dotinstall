48dc165b9729048ed03e595feb4a5af8f077f2fe
tamoriinu <tamoriinu@gmail.com>
2013-07-28 00:51:35 +0900

    必要なファイルを追加

---
+++ script/app.js
@@ -0,0 +1,23 @@
+(function(){
+
+//Model
+var Task = Backbone.Model.extend({
+	defaults: {
+		title: 'do something',
+		completed: false
+	}
+});
+
+var task = new Task();
+
+//View
+var TaskView = Backbone.View.extend({
+	tagName: 'li'
+});
+
+var taskView = new TaskView({model: task});
+
+console.log(taskView.$el);
+
+	
+})();
