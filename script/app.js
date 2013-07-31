add2882cc92e03f599b20b4b566c035b0e139e8a
tamoriinu <tamoriinu@gmail.com>
2013-08-01 01:26:00 +0900

    #03 make Model

--- script/app.js
+++ script/app.js
@@ -1,4 +1,17 @@
 (function(){
-
+	
+	//Model
+	var Task = Backbone.Model.extend({
+		defaults: {
+			title: "do something",
+			completed: false
+		}
+	});
+	
+	var task1 = new Task({
+		completed: true
+	});
+	
+	console.log(task1.toJSON());
 	
 })();
