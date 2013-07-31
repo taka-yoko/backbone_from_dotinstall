e629b1be4f6b278394f3a8b552d4242b4d20e883
tamoriinu <tamoriinu@gmail.com>
2013-08-01 01:12:21 +0900

    #02 prepare development

--- script/app.js
+++ script/app.js
@@ -1,23 +1,4 @@
 (function(){
 
-//Model
-var Task = Backbone.Model.extend({
-	defaults: {
-		title: 'do something',
-		completed: false
-	}
-});
-
-var task = new Task();
-
-//View
-var TaskView = Backbone.View.extend({
-	tagName: 'li'
-});
-
-var taskView = new TaskView({model: task});
-
-console.log(taskView.$el);
-
 	
 })();
