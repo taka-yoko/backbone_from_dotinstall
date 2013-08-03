5fc41ed4d8ab90f217502857c083b7c61d007348
tamoriinu <tamoriinu@gmail.com>
2013-08-03 22:43:11 +0900

    #11 make Collection View

--- script/app.js
+++ script/app.js
@@ -24,11 +24,22 @@
 	var Tasks = Backbone.Collection.extend({
 		model: Task
 	});
+	
+	var TasksView = Backbone.View.extend({
+		tagName: 'ul',
+		render: function(){
+			this.collection.each(function(task){
+				var taskView = new TaskView({model: task});
+				this.$el.append(taskView.render().el);
+			}, this);
+			return this;
+		}
+	});
+	
 	var tasks = new Tasks([
 		{
 			title: 'task1',
 			completed: true
-			
 		},
 		{
 			title: 'task2'
@@ -37,6 +48,9 @@
 			title: 'task3'
 		}
 	]);
-	console.log(tasks.toJSON());
-
+	
+	//console.log(tasks.toJSON());
+	var tasksView = new TasksView({collection: tasks});
+	$('#tasks').html(tasksView.render().el);
+	
 })();
