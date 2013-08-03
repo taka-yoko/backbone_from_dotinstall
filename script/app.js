c8767a9565dd24c1a3dae95770bd14414134eae3
tamoriinu <tamoriinu@gmail.com>
2013-08-03 20:58:41 +0900

    #06 make View

--- script/app.js
+++ script/app.js
@@ -5,30 +5,19 @@
 		defaults: {
 			title: "do something",
 			completed: false
-		},
-		validate: function(attrs){
-			if(_.isEmpty(attrs.title)){
-				return 'title must not be empty!';
-			}	
-		},
-		toggle: function(){
-			this.set('completed', !this.get('completed'));
 		}
 	});
 	
-	var task1 = new Task({
-		completed: true
+	var task = new Task();
+	
+	//View
+	var TaskView = Backbone.View.extend({
+		tagName: 'li'
+	});
+	var taskView = new TaskView({
+		model: task
 	});
 	
-	/*
-task1.set('title', 'newTitle');
-	var title = task1.get('title');
-	console.log(title);
-*/
-	
-	console.log(task1.toJSON());
-	task1.set({title: ''}, {validate: true});
-	//task1.toggle();
-	console.log(task1.toJSON());
-	
+	console.log(taskView.$el);
+		
 })();
