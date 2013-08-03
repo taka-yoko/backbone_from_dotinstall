92e65dbb35c69cb9e7423b9663322ccf3896e578
tamoriinu <tamoriinu@gmail.com>
2013-08-03 21:55:01 +0900

    #09 use events

--- script/app.js
+++ script/app.js
@@ -13,8 +13,12 @@
 	//View
 	var TaskView = Backbone.View.extend({
 		tagName: 'li',
-		//className: 'liClass',
-		//id: 'liId'
+		events: {
+			"click .command": "sayHello"	
+		},
+		sayHello: function(){
+			alert('hello');	
+		},
 		template: _.template($('#task-template').html()),
 		render: function(){
 			var template = this.template(this.model.toJSON());
