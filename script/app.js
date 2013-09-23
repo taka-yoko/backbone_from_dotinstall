8c99668ab2e038b05e104fbdf40588e34b190f6f
tamoriinu <tamoriinu@gmail.com>
2013-09-23 09:45:43 +0900

    #12 make To Do app

--- script/app.js
+++ script/app.js
@@ -1,40 +1,10 @@
 (function(){
 	
-	//Model
-	var Task = Backbone.Model.extend({
-		defaults: {
-			title: "do something",
-			completed: false
-		}
-	});
-	var task = new Task();
+	var Task = Backbone.Model.extend({});
+	var Tasks = Backbone.Collection.extend({ model: Task});
 	
-	//View
-	var TaskView = Backbone.View.extend({
-		tagName: 'li',
-		template: _.template($('#task-template').html()),
-		render: function(){
-			var template = this.template(this.model.toJSON());
-			this.$el.html(template);
-			return this;
-		}
-	});
-
-	//Collection
-	var Tasks = Backbone.Collection.extend({
-		model: Task
-	});
-	
-	var TasksView = Backbone.View.extend({
-		tagName: 'ul',
-		render: function(){
-			this.collection.each(function(task){
-				var taskView = new TaskView({model: task});
-				this.$el.append(taskView.render().el);
-			}, this);
-			return this;
-		}
-	});
+	var TaskView = Backbone.View.extend({});
+	var TasksView = Backbone.View.extend({});
 	
 	var tasks = new Tasks([
 		{
@@ -46,11 +16,8 @@
 		},
 		{
 			title: 'task3'
-		}
-	]);
-	
-	//console.log(tasks.toJSON());
-	var tasksView = new TasksView({collection: tasks});
-	$('#tasks').html(tasksView.render().el);
-	
+		}	
+	]);	
+		
+		
 })();
