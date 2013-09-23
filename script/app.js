7fe30f872fc6d25b75015ed420b25ff8f2db2420
tamoriinu <tamoriinu@gmail.com>
2013-09-23 09:53:16 +0900

    #13 make Model/View for task

--- script/app.js
+++ script/app.js
@@ -1,9 +1,23 @@
 (function(){
 	
-	var Task = Backbone.Model.extend({});
+	var Task = Backbone.Model.extend({
+		defaults: {
+			title: 'do something',
+			completed: false
+		}
+	});
 	var Tasks = Backbone.Collection.extend({ model: Task});
 	
-	var TaskView = Backbone.View.extend({});
+	var TaskView = Backbone.View.extend({
+		tagName: 'li',
+		template: _.template($('#task-template').html()),
+		render: function(){
+			var template = this.template(this.model.toJSON());
+			this.$el.html(template);
+			return this;
+		}
+		
+	});
 	var TasksView = Backbone.View.extend({});
 	
 	var tasks = new Tasks([
