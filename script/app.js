c4ce8ca3a870fe046cbaa94808acfe540074caa9
tamoriinu <tamoriinu@gmail.com>
2013-09-24 23:23:10 +0900

    #20 validate task

--- script/app.js
+++ script/app.js
@@ -4,6 +4,16 @@
 		defaults: {
 			title: 'do something',
 			completed: false
+		},
+		validate: function(attrs){
+			if(_.isEmpty(attrs.title)){
+				return 'title must not be empty';
+			}
+		},
+		initialize: function(){
+			this.on('invalid', function(model, error){
+				$('#error').html(error);
+			});
 		}
 	});
 	
